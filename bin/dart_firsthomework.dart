8e29e4f872fce4f4b3a19b6c16ca15607b9d98c1
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-10-07 16:26:52 +0600

    first commit

---
+++ bin/dart_firsthomework.dart
@@ -0,0 +1,7 @@
+import 'package:dart_firsthomework/dart_firsthomework.dart' as dart_firsthomework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_firsthomework.calculate()}!');
+}
+print('abdibek');
+}
